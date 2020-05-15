4ab0c34cdc817cb25ad228b9c4731e56f32bb7af
Cody Um <yunsuum@gmail.com>
2020-05-15 13:27:32 +0200

    update on react

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -3,9 +3,14 @@ import ReactDOM, { render } from "react-dom";
 import data from "./data.json";
 import Cell from "./Cell.js";
 
+
 function App (){
     return(
-        <div>{data.body.map(player => React.createElement(Cell,player))}</div>
+        <table className= "ranked_table">
+            <tbody>
+            {data.body.map(player => React.createElement(Cell,player))}
+            </tbody>
+            </table>
     );
 }
 export default App;
--- react_test/src/Cell.js
+++ react_test/src/Cell.js
@@ -1,8 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import image from "../public/asset/kobeBryant.jpg";
 
 function Cell (props){
-    return <div>{props.name}</div>
+
+    return (
+    <tr className = "row">
+        <th>1</th>
+        <th>
+            <div className = "image_border">
+                <img src= {props.image} className = "profile_pic" alt="" />
+            </div></th>
+        <th><div className = "name">{props.name}</div></th>
+        <th>
+            <a className = "upVote">🔺</a>
+            <a className = "downVote">🔻</a>
+        </th>
+        <th className = "count">{props.count}</th>
+    </tr>
+    );
 }
 
 export default Cell;
