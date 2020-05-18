e92c8a2f3982d1f9991ac8a7c86093d53985e60c
Cody Um <yunsuum@gmail.com>
2020-05-18 17:51:01 +0200

    Updated the server and react to allow dynamic rank page generation depending on the API. Originally, only nba document could be accessed since it was the index

--- react_with_express/express_Server/server.js
+++ react_with_express/express_Server/server.js
@@ -15,9 +15,10 @@ let itemSchema = new mongoose.Schema({
     count: Number
 })
 
-const Item = new mongoose.model("Item", itemSchema);
 
-app.get("/nba", (req, res) => {
+app.get("/ranked/:item", (req, res) => {
+    let item = req.params.item;
+    const Item = new mongoose.model(item, itemSchema);
     Item.find({}).sort({count:-1}).exec(function(err,docs){
         if(err){
             return console.log(err);
@@ -26,10 +27,25 @@ app.get("/nba", (req, res) => {
     })
 });
 
-app.post("/nba/:player",function(req,res){
+app.post("/ranked/:item/upvote/:player",function(req,res){
+    let item = req.params.item.toString;
+    const Item = new mongoose.model(item, itemSchema);
     let player = req.params.player;
     console.log("accessed");
     Item.findOneAndUpdate({name:player}, {$inc: {count:1}},function(err,data){
+        if(err){
+            return console.log(err);
+        }
+        console.log("UPDATED: "+data);
+    });
+})
+app.post("/ranked/:item/downvote/:player",function(req,res){
+    let item = req.params.item;
+    const Item = new mongoose.model(item, itemSchema);
+
+    let player = req.params.player;
+    console.log("accessed");
+    Item.findOneAndUpdate({name:player}, {$inc: {count:-1}},function(err,data){
         if(err){
             return console.log(err);
         }
--- react_with_express/src/Cell.js
+++ react_with_express/src/Cell.js
@@ -3,34 +3,56 @@ import ReactDOM from "react-dom";
 import posed from "react-pose";
 //import image from "../public/asset/kobeBryant.jpg";
 
+
+// VOTE ANIMATION USING POSE JS
 const Vote = posed.div({
     hidden: { left: "0px" },
     visible: {
         left: "-75px",
         transition: {
-            width: { ease: "easeInAndOut", duration: 300 }
+            width: { ease: "easeInAndOut", duration: 200 }
         }
     }
 });
 
 class Cell extends React.Component {
 
+    // CONSTRUCTOR and Initializes the STATE.
     constructor(props) {
         super(props);
         this.state = {
             isVisible: false,
             count: this.props[0].count,
-            clicked: false
+            clicked: false,
+            clickCount:0,
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
+    // vote CLICKED HANDLER to Toggle animation 
     handleClick(event) {
-        this.setState({
-            isVisible: !this.state.isVisible
-        });
+        
+        if(this.state.clickCount === 0){
+            this.setState({
+                isVisible: !this.state.isVisible,
+                clickCount: 1
+            });
+        }else if(this.state.clickCount === 1){
+            this.setState({
+                isVisible1: !this.state.isVisible1,
+                clickCount: 2
+            });
+        }else{
+            this.setState({
+                isVisible: false,
+                isVisible1: false,
+                clickCount: 0
+            });
+        }
+        
     }
 
+    // POSTS to Server that the item is voted
     componentDidMount() {
         const current = ReactDOM.findDOMNode(this);
         let button = current.querySelector(".upVote");
@@ -38,7 +60,8 @@ class Cell extends React.Component {
         console.log(player);
         button.addEventListener("click", (e) => {
             if (this.state.clicked === false) {
-                fetch("/nba/" + player, {
+                console.log("SENDING POST TO "+ player)
+                fetch("ranked/nba/upvote/" + player, {
                     method: "post",
                     body: {
                         "name": player
@@ -46,20 +69,29 @@ class Cell extends React.Component {
                 });
                 this.setState({ count: this.state.count + 1, clicked: true });
             }else{
+                console.log("SENDING POST TO "+ player)
+                fetch("ranked/nba/downvote/" + player, {
+                    method: "post",
+                    body: {
+                        "name": player
+                    }
+                });
                 this.setState({ count: this.state.count -1, clicked: false});
             }
         })
     }
 
-
+    //RENDER the Row
     render() {
         const { isVisible } = this.state;
+        const { isVisible1 } = this.state;
         return (
 
             <tr className="row">
                 <th className="voteContainer">
-
-                    <Vote className="vote" pose={isVisible ? "visible" : "hidden"}></Vote>
+                    <Vote className="vote1" pose={isVisible ? "visible" : "hidden"}></Vote>
+                    <Vote className="vote2" pose={isVisible1 ? "visible" : "hidden"}></Vote>
+                    <Vote className="vote3" pose={isVisible ? "visible" : "hidden"}></Vote>
                 </th>
                 <th>{this.props[1]}</th>
                 <th>
@@ -70,8 +102,8 @@ class Cell extends React.Component {
                     </div></th>
                 <th><div className="name">{this.props[0].name}</div></th>
                 <th>
-                    <a className="upVote">
-                        <img src="/asset/vote_btn.svg" onClick={this.handleClick}></img>
+                    <a className="upVote" onClick={this.handleClick}>
+                        <img src="/asset/vote_btn.svg" className = "voteSVG"></img>
                     </a>
                 </th>
                 <th className="count">{this.state.count}</th>
