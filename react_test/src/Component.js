ce2e7c21fb6c9c8b3289151554899c29bab4d131
Cody Um <yunsuum@gmail.com>
2020-05-26 18:15:54 +0200

    Optimizing folder organization for deployment

--- react_test/src/Component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM, { render } from "react-dom";
-import Cell from "./Cell.js";
-
-function Component(props){
-    return React.createElement(Cell,props);
-}
-
-export default Component;
