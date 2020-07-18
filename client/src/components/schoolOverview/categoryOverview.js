b5e794e33db9afd4df9caced55edc0e140444700
Cody Um <yunsuum@gmail.com>
2020-07-19 08:47:05 +0900

    two branches merged

--- client/src/components/schoolOverview/categoryOverview.js
+++ client/src/components/schoolOverview/categoryOverview.js
@@ -3,30 +3,31 @@ import CategoryCell from "./categoryCell.js";
 import { Link } from "react-router-dom";
 
 function CategoryOverview(props) {
-  if (!props.items) {
-    return null;
-  } else {
-    let items = Object.values(props.items);
-    console.log(items);
-
     if (!props.items) {
         return null;
     } else {
-
-        const categoryLowered = props.category.toLowerCase();
         let items = Object.values(props.items);
         console.log(items);
 
-        return (
-            <Link to = {`/ranked/${props.school}/${props.school+categoryLowered}`}>
-                <table className="categoryOverviewContainer">
-                    <tbody>
-                        <tr className = "header"><td>{`${props.category} Overview`}</td></tr>
-                        {items.map((item) => React.createElement(CategoryCell, [item]))}
-                    </tbody>
-                </table>
-            </Link>
-        )
+        if (!props.items) {
+            return null;
+        } else {
+
+            const categoryLowered = props.category.toLowerCase();
+            let items = Object.values(props.items);
+            console.log(items);
+
+            return (
+                <Link to={`/ranked/${props.school}/${props.school + categoryLowered}`}>
+                    <table className="categoryOverviewContainer">
+                        <tbody>
+                            <tr className="header"><td>{`${props.category} Overview`}</td></tr>
+                            {items.map((item) => React.createElement(CategoryCell, [item]))}
+                        </tbody>
+                    </table>
+                </Link>
+            )
+        }
     }
 }
 export default CategoryOverview;
--- client/src/components/schoolOverview/schoolOverview.js
+++ client/src/components/schoolOverview/schoolOverview.js
@@ -4,20 +4,20 @@ import CategoryOverview from "./categoryOverview.js";
 
 class SchoolOverview extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: { library: [] },
-            school: {},
-            title: props.match.params.school,
-            overallRating: {
-                library: 0,
-                dininghall: 0,
-                gym: 0,
-                residentialhall: 0
-            }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: { library: [] },
+      school: {},
+      title: props.match.params.school,
+      overallRating: {
+        library: 0,
+        dininghall: 0,
+        gym: 0,
+        residentialhall: 0
+      }
     }
+  }
 
 
   async componentDidMount() {
@@ -34,62 +34,60 @@ class SchoolOverview extends React.Component {
     this.setState({ school: school[0] });
   }
 
-    render() {
-        const schoolLowered = this.props.match.params.school.toLowerCase();
-        return (
-            <div>
-                <div id="schoolBanner">
-                    <div id="imgBannerGradient" />
-                    <div id="imgBannerOverlay">
-                        <div id="imgBannerOverlayTitle" className = "noBreak">
-                            {this.state.title}
-                        </div>
-                        <div id="imgBannerOverlaySub">
-                            {`Public: ${this.state.school.public} / State: ${this.state.school.state}`}
-                        </div>
-                    </div>
-
-                    <img id="imgBanner" src={`https://campusranked.s3.us-east-2.amazonaws.com/${schoolLowered}/${schoolLowered}_banner.jpg`}/>
-                    <div style={{ width: "100%", height: "30px", "background-color": "black" }}></div>
-                </div>
-                <div style={{ height: "30px" }} />
-                {/*<MapComponent latitude = {this.state.school.latitude} longitude = {this.state.school.longitude}/>*/}
-                <h3> Top Buildings In Each Category</h3>
-                <div id="rankOverviewContainer">
-                    <CategoryOverview school={schoolLowered} category="Residentialhall" items={this.state.data.residentialhall} />
-                    <CategoryOverview school={schoolLowered} category="Library" items={this.state.data.library} />
-                    <CategoryOverview school={schoolLowered} category="Dininghall" items={this.state.data.dininghall} />
-                    <CategoryOverview school={schoolLowered} category="Gym" items={this.state.data.gym} />
-
-                </div>
+  render() {
+    const schoolLowered = this.props.match.params.school.toLowerCase();
+    return (
+      <div>
+        <div id="schoolBanner">
+          <div id="imgBannerGradient" />
+          <div id="imgBannerOverlay">
+            <div id="imgBannerOverlayTitle" className="noBreak">
+              {this.state.title}
+            </div>
+            <div id="imgBannerOverlaySub">
+              {`Public: ${this.state.school.public} / State: ${this.state.school.state}`}
             </div>
           </div>
 
-          <img
-            id="imgBanner"
-            src="https://campusranked.s3.us-east-2.amazonaws.com/uwmadison/uwmadison_banner.jpg"
-          />
+          <img id="imgBanner" src={`https://campusranked.s3.us-east-2.amazonaws.com/${schoolLowered}/${schoolLowered}_banner.jpg`} />
+          <div style={{ width: "100%", height: "30px", "background-color": "black" }}></div>
         </div>
         <div style={{ height: "30px" }} />
+        {/*<MapComponent latitude = {this.state.school.latitude} longitude = {this.state.school.longitude}/>*/}
+        <h3> Top Buildings In Each Category</h3>
         <div id="rankOverviewContainer">
-          <h2> Top Buildings In Each Category</h2>
-          <CategoryOverview
-            school={this.props.match.params.school}
-            category="Library"
-            items={this.state.data.library}
-          />
-          <CategoryOverview
-            school={this.props.match.params.school}
-            category="Dininghall"
-            items={this.state.data.dininghall}
-          />
-          <CategoryOverview
-            school={this.props.match.params.school}
-            category="Gym"
-            items={this.state.data.gym}
-          />
+          <CategoryOverview school={schoolLowered} category="Residentialhall" items={this.state.data.residentialhall} />
+          <CategoryOverview school={schoolLowered} category="Library" items={this.state.data.library} />
+          <CategoryOverview school={schoolLowered} category="Dininghall" items={this.state.data.dininghall} />
+          <CategoryOverview school={schoolLowered} category="Gym" items={this.state.data.gym} />
+
         </div>
+      <div>
+        <img
+          id="imgBanner"
+          src="https://campusranked.s3.us-east-2.amazonaws.com/uwmadison/uwmadison_banner.jpg"
+        />
       </div>
+      <div style={{ height: "30px" }} />
+      <div id="rankOverviewContainer">
+        <h2> Top Buildings In Each Category</h2>
+        <CategoryOverview
+          school={this.props.match.params.school}
+          category="Library"
+          items={this.state.data.library}
+        />
+        <CategoryOverview
+          school={this.props.match.params.school}
+          category="Dininghall"
+          items={this.state.data.dininghall}
+        />
+        <CategoryOverview
+          school={this.props.match.params.school}
+          category="Gym"
+          items={this.state.data.gym}
+        />
+      </div>
+      </div >
     );
   }
 }
