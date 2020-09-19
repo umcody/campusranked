e51e49713490ba879f0881906d06a6d8c11b0dad
Cody Um <yunsuum@gmail.com>
2020-09-18 21:52:30 -0500

    update

--- client/src/components/admin/createSchool.js
+++ client/src/components/admin/createSchool.js
@@ -34,42 +34,44 @@ function CreateSchool() {
     }
 
     return (
-        <div style={{ "position": "absolute", "top": "40%", "left": "50%", "transform": "translate(-50%,-50%)" }}>
-            <form>
-                <div style={{ display: "inline-block", "vertical-align":"top"}}>
-                    <p>School Name</p>
-                    <input type="text" onChange={(e) => { setSchoolName(e.target.value) }} placeholder="School Name"></input>
-                    <p>School Full Name</p>
-                    <input type="text" onChange={(e) => { setSchoolFullName(e.target.value) }} placeholder="School Full Name" ></input>
-                    <p>State</p>
-                    <input type="text" onChange={(e) => { setState(e.target.value) }} placeholder="State"></input>
+        <div className="onePage">
+            <div style={{ "position": "absolute", "top": "40%", "left": "50%", "transform": "translate(-50%,-50%)" }}>
+                <form>
+                    <div style={{ display: "inline-block", "vertical-align": "top" }}>
+                        <p>School Name</p>
+                        <input type="text" onChange={(e) => { setSchoolName(e.target.value) }} placeholder="School Name"></input>
+                        <p>School Full Name</p>
+                        <input type="text" onChange={(e) => { setSchoolFullName(e.target.value) }} placeholder="School Full Name" ></input>
+                        <p>State</p>
+                        <input type="text" onChange={(e) => { setState(e.target.value) }} placeholder="State"></input>
 
-                    <p>Latitude</p>
-                    <input type="text" onChange={(e) => { setLatitude(e.target.value) }} placeholder="Latitude"></input>
-                    <p>Longitude</p>
-                    <input type="text" onChange={(e) => { setLongitude(e.target.value) }} placeholder="Longitude"></input>
+                        <p>Latitude</p>
+                        <input type="text" onChange={(e) => { setLatitude(e.target.value) }} placeholder="Latitude"></input>
+                        <p>Longitude</p>
+                        <input type="text" onChange={(e) => { setLongitude(e.target.value) }} placeholder="Longitude"></input>
 
-                    <p>Public</p>
-                    <input type="radio" value="true" onClick={(e) => { setPublic(Boolean(e.target.value)) }} placeholder="Public"></input>
-                    <p>Private</p>
-                    <input type="radio" value="false" onClick={(e) => { setPublic(Boolean(e.target.value)) }} placeholder="Private"></input>
+                        <p>Public</p>
+                        <input type="radio" value="true" onClick={(e) => { setPublic(Boolean(e.target.value)) }} placeholder="Public"></input>
+                        <p>Private</p>
+                        <input type="radio" value="false" onClick={(e) => { setPublic(Boolean(e.target.value)) }} placeholder="Private"></input>
 
-                </div>
+                    </div>
 
-                <div style={{ display: "inline-block", "vertical-align":"top"}}>
-                    <h6>Here you put the list of buildings. *** Seperate the buildings by "," (NO SPACE AFTER COMMA) ***</h6>
+                    <div style={{ display: "inline-block", "vertical-align": "top" }}>
+                        <h6>Here you put the list of buildings. *** Seperate the buildings by "," (NO SPACE AFTER COMMA) ***</h6>
 
-                    <p>Gym</p>
-                    <input type="text" onChange={(e) => { setGym(e.target.value.split(",")) }} placeholder="Gym Buildings"></input>
-                    <p>ResidentialHall</p>
-                    <input type="text" onChange={(e) => { setResidentialhall(e.target.value.split(",")) }} placeholder="ResidentialHall Buildings"></input>
-                    <p>Dining Hall</p>
-                    <input type="text" onChange={(e) => { setDininghall(e.target.value.split(",")) }} placeholder="Dininghall Buildings"></input>
-                    <p>Library</p>
-                    <input type="text" onChange={(e) => { setLibrary(e.target.value.split(",")) }} placeholder="Library Buildings"></input>
-                </div>
-            </form>
-            <p onClick={submit}> SUBMIT</p>
+                        <p>Gym</p>
+                        <input type="text" onChange={(e) => { setGym(e.target.value.split(",")) }} placeholder="Gym Buildings"></input>
+                        <p>ResidentialHall</p>
+                        <input type="text" onChange={(e) => { setResidentialhall(e.target.value.split(",")) }} placeholder="ResidentialHall Buildings"></input>
+                        <p>Dining Hall</p>
+                        <input type="text" onChange={(e) => { setDininghall(e.target.value.split(",")) }} placeholder="Dininghall Buildings"></input>
+                        <p>Library</p>
+                        <input type="text" onChange={(e) => { setLibrary(e.target.value.split(",")) }} placeholder="Library Buildings"></input>
+                    </div>
+                </form>
+                <p onClick={submit}> SUBMIT</p>
+            </div>
         </div>
     );
 }
